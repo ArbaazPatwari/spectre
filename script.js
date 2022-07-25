3b99a3a11e92756ed277fb4f89a8e0c9e4958633
Arbaaz Patwari <arbaazpatwari@gmail.com>
2022-07-25 05:39:22 +0100

    Content of my website
    I aim to make a github pages website with these files. It is unfinished but I am continuing to work on it.

---
+++ script.js
@@ -0,0 +1,7 @@
+// Simple JavaScript function to travel to the top of the page 
+topbutton = document.getElementsByClass("footer-button");
+
+function pageTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
